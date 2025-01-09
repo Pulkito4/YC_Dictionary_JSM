2b89b94d1f6f1997e3bb6862029b7cef62553ea3
Pulkit Goyal <pulkitcgoyal@gmail.com>
2025-01-10 00:58:28 +0530

    implement search form using next form

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,24 @@
-export default function Home() {
-  return (
-    <>
-    <h1 className="text-2xl">HOME</h1>
-    </>
-  );
+import SearchForm from "../../components/SearchForm";
+
+export default async function Home({
+	searchParams,
+}: {
+	searchParams: Promise<{ query?: string }>;
+}) {
+	const query = (await searchParams).query;
+	return (
+		<>
+			<section className="pink_container">
+				<h1 className="heading">
+					Pitch Your Startup, <br />
+					Connect With Entrepreneurs
+				</h1>
+				<p className="sub-heading !max-w-3xl">
+					Submti Ideas, Vote on Pitches, and Get Notices in Virtual
+					Competitions
+				</p>
+				<SearchForm query={query}/>
+			</section>
+		</>
+	);
 }
---
+++ components/SearchForm.tsx
@@ -0,0 +1,28 @@
+import { Search } from "lucide-react";
+import SearchFormReset from "./SearchFormReset";
+import Form from "next/form";
+
+const SearchForm = ({query}:{query?:string}) => {
+
+	return (
+		<Form action="/search" scroll={false} className="search-form">
+			{/* On submission, the input value will be appended to 
+          the URL, e.g. /search?query=abc */}
+			<input
+				name="query"
+				defaultValue={query}
+				className="search-input"
+				placeholder="Search Startups"
+			/>
+			<div className=" flex gap-2">
+				{query && <SearchFormReset />}
+
+				<button type="submit" className="search-btn text-white">
+					<Search className="size-5"/>
+				</button>
+			</div>
+		</Form>
+	);
+};
+
+export default SearchForm;
